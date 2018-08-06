2d128f96ae2a5c9e883639523d8aae690f0a6bd5
Hiroki Kato <hkato@sas.upenn.edu>
2018-08-06 16:29:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,14 +12,13 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    # binding.pry
     students_array.each do |student|
       Student.new(student)
     end      
   end
 
   def add_student_attributes(attributes_hash)
-    
+    binding.pry
   end
 
   def self.all
