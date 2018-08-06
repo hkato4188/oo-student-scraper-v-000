13315cf625f3d40173d65ce26402835ed10aef03
Hiroki Kato <hkato@sas.upenn.edu>
2018-08-06 15:51:38 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -36,7 +36,6 @@ class Scraper
       end
     student[:profile_quote] = profile_page.css("div.profile-quote").text if profile_page.css("div.profile-quote")
     student[:bio] = profile_page.css("div.description-holder").text
-    
     end
     student
   end
