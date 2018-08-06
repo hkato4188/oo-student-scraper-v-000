61becf3b8066de677d6cb393ef51e49ecf061ad8
Hiroki Kato <hkato@sas.upenn.edu>
2018-08-06 15:26:49 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,9 +20,8 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     #returns hash where key/value pairs describe individual student
     profile_page = Nokogiri::HTML(open(profile_url))
-    binding.pry
-    # profile_page.css("div.social-icon-container").children.css("a").map{|link| link.("href).value}
-    #   
+    links = profile_page.css("div.social-icon-container").children.css("a").map{|link| link.("href).value}
+    binding.pry   
   end
 
 end
