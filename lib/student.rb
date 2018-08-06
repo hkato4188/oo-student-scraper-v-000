cbf4302160276a7aaf7b369e1352326f54bf4c24
Hiroki Kato <hkato@sas.upenn.edu>
2018-08-06 16:40:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,7 @@ class Student
 
   def add_student_attributes(attributes_hash)
     binding.pry
-    student_hash.each do |key, value|
+    attributes_hash.each do |key, value|
       self.send("#{key}=", value)
     end
   end
